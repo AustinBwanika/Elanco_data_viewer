6635afa9d1f48fd5a6bff351d5dcfff9ce88b0c8
AustinBwanika <austin.kasekende12@gmail.com>
2024-02-26 12:38:11 +0000

    final edits

--- appconfig/views.py
+++ appconfig/views.py
@@ -1,5 +1,13 @@
+import datetime
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+import json
+
+
 from django.shortcuts import render
 from .api_data import ApiData
+from datetime import datetime, timedelta
+
 # Create your views here.
 
 def show_home(request):
@@ -10,14 +18,20 @@ def show_visuals(request):
     data = api_data.get_data()
     consumption = api_data.consumption_per_day()
 
-    labels = [date for date, k in consumption]
-    usage= [quantity for k, quantity in consumption]
+    # labels = [date for date, k in consumption]
+    # usage= [quantity for k, quantity in consumption]
+    test_consumption = [
+        (datetime.now() - timedelta(days=i), i * 10) for i in range(9)
+    ]
+
+    labels = [date.strftime("%Y-%m-%d") for date, quantity in test_consumption]
+    usage = [quantity for date, quantity in test_consumption]
 
     graph_data = {
         'labels': labels,
         'usage': usage
     }
-    return render(request, 'visuals.html', context={'data': data, 'consumption': consumption, 'graph_data': graph_data})
+    return render(request, 'visuals.html', context={'data': data, 'consumption': consumption, 'graph_data': json.dumps(graph_data)})
 
 def show_data(request):
     api_data = ApiData()
